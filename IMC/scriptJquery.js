2b1d1b5411ea77d6e2a844797c21f309b7120b81
msmelo <marcel.msmelo@gmail.com>
2016-11-11 16:36:54 -0200

    Aula Validação Forms

--- IMC/scriptJquery.js
+++ IMC/scriptJquery.js
@@ -1,20 +1,48 @@
 $(document).ready(function(){
-	$("#button").click(function(){
+	$('#button').click(function(){
 		let nomeUsuario = $("#nome").val();
 		let pesoUsuario = $("#peso").val();
+		let alturaUsuario = $("#altura").val();
+		let idadeUsuario = $("#idade").val();
+		let dtNascUsuario = $("#dtNasc").val();
 
-		alert("Nome: "+nomeUsuario);
-		alert("Peso: "+pesoUsuario);
 
-		let resultado = pesoUsuario*2;
-		alert("Resultado: "+resultado);
+		let imc = pesoUsuario/(alturaUsuario*alturaUsuario);
 
-		if(resultado > 100){
-			$("#resultado").html("Resultado > 100: "+resultado);
+		$("#exibeNome").html("Nome: " + nomeUsuario);
+		$("#exibePeso").html("Peso: " + pesoUsuario);
+		$("#exibeAltura").html("Altura: " + alturaUsuario);
+		$("#exibeIdade").html("Idade: " + idadeUsuario);
+		$("#exibeDtNasc").html("Data Nascimento: " + inverteData(dtNascUsuario));
+
+		if(idadeUsuario < 16){
+			$("#resultado").html("Você é novinho, não tem IMC");
+			return false;
 		}
-		else{
-			$("#resultado").html("Resultado < 100: "+resultado);
+
+		if(imc < 17){
+			$("#resultado").html("IMC: "+imc+" - Muito abaixo do peso");
+		}else if(imc>=17 && imc <= 18.49){
+			$("#resultado").html("IMC: "+imc+" - Abaixo do peso");
+		}
+		else if(imc>=18.5 && imc <= 24.99){
+			$("#resultado").html("IMC: "+imc+" - Peso Normal");
+		}
+		else if(imc>=25 && imc <= 29.99){
+			$("#resultado").html("IMC: "+imc+" - SobrePeso");
+		}
+		else if(imc>=30 && imc <= 34.99){
+			$("#resultado").html("IMC: "+imc+" - Obesidade I");
+		}
+		else if(imc>=35 && imc <= 39.99){
+			$("#resultado").html("IMC: "+imc+" - Obesidade II (Severo)");
+		}else if(imc>=40){
+			$("#resultado").html("IMC: "+imc+" - Obesidade III (Mórbida)");
 		}
 	}
-	);
+	)
 });
+function inverteData(date){
+	let dataVetor = date.split('-');
+	return dataVetor[2]+'/'+dataVetor[1]+'/'+dataVetor[0]
+}
--- IMC/scripts.js
+++ IMC/scripts.js
@@ -1,17 +1,63 @@
-function clicar(){
+function validar(){
 	let nomeUsuario = document.querySelector("#nome").value;
 	let pesoUsuario = document.querySelector("#peso").value;
+	let alturaUsuario = document.querySelector("#altura").value;
+	let idadeUsuario = document.querySelector("#idade").value;
+	let dtNascUsuario = document.querySelector("#dtNasc").value;
 
-	alert("Nome: "+nomeUsuario);
-	alert("Peso: "+pesoUsuario);
 
-	let resultado = pesoUsuario*2;
-	alert("Resultado: "+resultado);
+	let imc = pesoUsuario/(alturaUsuario*alturaUsuario);
 
-	if(resultado > 100){
-		document.querySelector("#resultado").innerHTML = "Resultado > 100: "+resultado;
+	document.querySelector("#exibeNome").innerHTML = "Nome: " + nomeUsuario;
+	document.querySelector("#exibePeso").innerHTML = "Peso: " + pesoUsuario;
+	document.querySelector("#exibeAltura").innerHTML = "Altura: " + alturaUsuario;
+	document.querySelector("#exibeIdade").innerHTML = "Idade: " + idadeUsuario;
+	document.querySelector("#exibeDtNasc").innerHTML = "Data Nascimento: " + inverteData(dtNascUsuario);
+
+	if(idadeUsuario < 16){
+		document.querySelector("#resultado").innerHTML = "Você é novinho, não tem IMC";
+		return false;
 	}
-	else{
-		document.querySelector("#resultado").innerHTML = "Resultado < 100: "+resultado;
+
+	if(imc < 17){
+		document.querySelector("#resultado").innerHTML = "IMC: "+imc+" - Muito abaixo do peso";
+	}else if(imc>=17 && imc <= 18.49){
+		document.querySelector("#resultado").innerHTML = "IMC: "+imc+" - Abaixo do peso";
 	}
+	else if(imc>=18.5 && imc <= 24.99){
+		document.querySelector("#resultado").innerHTML = "IMC: "+imc+" - Peso Normal";
+	}
+	else if(imc>=25 && imc <= 29.99){
+		document.querySelector("#resultado").innerHTML = "IMC: "+imc+" - SobrePeso";
+	}
+	else if(imc>=30 && imc <= 34.99){
+		document.querySelector("#resultado").innerHTML = "IMC: "+imc+" - Obesidade I";
+	}
+	else if(imc>=35 && imc <= 39.99){
+		document.querySelector("#resultado").innerHTML = "IMC: "+imc+" - Obesidade II (Severo)";
+	}else if(imc>=40){
+		document.querySelector("#resultado").innerHTML = "IMC: "+imc+" - Obesidade III (Mórbida)";
+	}
+	return false;
+}
+
+
+function inverteData(date){
+	let dataVetor = date.split('-');
+	return dataVetor[2]+'/'+dataVetor[1]+'/'+dataVetor[0]
+}
+
+
+
+function validaNome(){
+	let nome = document.dados.nome.value;
+	let regex = /^[a-zA-Z ]{3,20}$/;
+	if(regex.test(nome)){
+		document.dados.nome.classList.remove("erro")
+		document.dados.nome.classList.add("certo")
+	}else{
+		document.dados.nome.classList.remove("certo")
+		document.dados.nome.classList.add("erro")
+	}
+
 }
